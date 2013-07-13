947a83af2ccdca2d65a94308a27f279262963a7a
vweng <vweng@idemfactor.com>
2013-07-13 14:51:55 -0400

    fixed heyzap adapter

--- extras/src/com/mopub/mobileads/HeyZapInterstitial.java
+++ extras/src/com/mopub/mobileads/HeyZapInterstitial.java
@@ -4,6 +4,7 @@ import java.util.Map;
 
 import android.app.Activity;
 import android.content.Context;
+import android.util.Log;
 
 import com.heyzap.sdk.ads.InterstitialOverlay;
 import com.heyzap.sdk.ads.OnAdDisplayListener;
@@ -24,6 +25,8 @@ class HeyZapInterstitial extends CustomEventInterstitial implements OnAdDisplayL
 	protected void loadInterstitial(Context context,
 			CustomEventInterstitialListener interstitialListener,
 			Map<String, Object> localExtras, Map<String, String> serverExtras) {
+		Log.d("HeyZap","loadInterstitial");
+
 		mInterstitialListener = interstitialListener;
 
 		if (context instanceof Activity) {
@@ -44,9 +47,39 @@ class HeyZapInterstitial extends CustomEventInterstitial implements OnAdDisplayL
 		 * Remember to call HeyzapLib.start(this, HeyzapLib.FLAG_NO_HEYZAP_INSTALL_SPLASH);
 		 * in main activity's onCreate()
 		 */
+		// do not use load(context, listener) as it won't set listener after initial call
 		InterstitialOverlay.setDisplayListener(this);
+
+		if (InterstitialOverlay.isAvailable()) {
+			// already
+			this.onAvailable();
+		} else {
+			if	(InterstitialOverlay.getInstance() == null)
+				InterstitialOverlay.load(context, this);
+			InterstitialOverlay.fetch(); // fetch won't do anything if loading is in process.
+		}
+
 	}
 
+	@Override
+	protected void showInterstitial() {
+		Log.d("HeyZap","showInterstitial");
+
+		if (InterstitialOverlay.isAvailable()) {
+			InterstitialOverlay.display(contextActivity);
+
+	    }
+		
+	}
+
+	@Override
+	protected void onInvalidate() {
+		InterstitialOverlay.setDisplayListener(null);
+		contextActivity = null;
+		
+	}
+
+	
 	@Override
 	public void onShow() {
 		mInterstitialListener.onInterstitialShown();
@@ -69,10 +102,13 @@ class HeyZapInterstitial extends CustomEventInterstitial implements OnAdDisplayL
 	public void onFailedToShow() {
 		mInterstitialListener.onInterstitialFailed(MoPubErrorCode.INTERNAL_ERROR);
 		
+		Log.w("HeyZap","Failed to show");
+
 	}
 
 	@Override
 	public void onAvailable() {
+		Log.d("HeyZap","onAvailable");
 		mInterstitialListener.onInterstitialLoaded();
 		
 	}
@@ -81,19 +117,8 @@ class HeyZapInterstitial extends CustomEventInterstitial implements OnAdDisplayL
 	public void onFailedToFetch() {
 		mInterstitialListener.onInterstitialFailed(MoPubErrorCode.NETWORK_NO_FILL);
 		
-	}
+		Log.w("HeyZap","Failed to fetch");
 
-	@Override
-	protected void showInterstitial() {
-		InterstitialOverlay.display(contextActivity);
-		
-	}
-
-	@Override
-	protected void onInvalidate() {
-		InterstitialOverlay.setDisplayListener(null);
-		contextActivity = null;
-		
 	}
 
 	
